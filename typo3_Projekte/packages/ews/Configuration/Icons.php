1bcff1212c9f231dd77e27efcd81afc8059807ae
hoodaddeveloper <mohammadkhaffas@gmail.com>
2023-01-02 12:08:50 +0100

    .

--- typo3_Projekte/packages/ews/Configuration/Icons.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'ews-plugin-ews' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:ews/Resources/Public/Icons/user_plugin_ews.svg'
-    ],
-];
--- typo3_Projekte/packages/ews/Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined('TYPO3') || die();
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('ews', 'Configuration/TypoScript', 'my first plugin');
